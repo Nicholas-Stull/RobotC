ef375ac17f27e185449b6d58a62249d598dad0f9
Nicholas <Nicholass077@gmail.com>
2018-11-29 16:43:18 -0500

    Activity 7.1

---
+++ Activitys/7/7.1.c
@@ -0,0 +1,23 @@
+#pragma config(Sensor, in1,    lineFollower,   sensorLineFollower)
+#pragma config(Sensor, in2,    pot,            sensorPotentiometer)
+#pragma config(Sensor, in3,    lightSensor,    sensorReflection)
+#pragma config(Sensor, dgtl1,  LimitSwitch,    sensorTouch)
+#pragma config(Sensor, dgtl2,  bumpSwitch,     sensorTouch)
+#pragma config(Sensor, dgtl3,  quad,           sensorQuadEncoder)
+#pragma config(Sensor, dgtl5,  sonar,          sensorSONAR_inch)
+#pragma config(Sensor, dgtl12, green,          sensorLEDtoVCC)
+#pragma config(Motor,  port1,           flashLight,    tmotorVexFlashlight, openLoop, reversed)
+#pragma config(Motor,  port2,           rightMotor,    tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port3,           leftMotor,     tmotorVex393_MC29, openLoop)
+#pragma config(Motor,  port4,           centerMotor,   tmotorServoContinuousRotation, openLoop)
+task main()
+{
+int j;          /*variable*/
+int speed;
+
+wait(1.0);
+while(1)
+{unti
+
+
+}

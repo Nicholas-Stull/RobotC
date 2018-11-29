cb7a8ca1c0070bd4a15ca079cef7736fa728de9e
Nicholas <Nicholass077@gmail.com>
2018-11-29 16:46:19 -0500

    Working On LED part

---
+++ Activitys/Dragster/Dragster2.c
@@ -0,0 +1,16 @@
+
+task main()
+{
+	untilBump(startButton);
+	turnLEDOn(12);
+	wait(1);
+	turnLEDOff(12);
+	turnLEDOn(11);
+	wait(1);
+	turnLEDOff(11);
+	turnLEDOn(10);
+	wait(1);
+	turnLEDOff(10);
+
+
+}
